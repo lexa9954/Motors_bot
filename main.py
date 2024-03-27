3f8d2408fb96c34f2fbfa4d6bc18a3296e81780e
lexa9954 <32564058+lexa9954@users.noreply.github.com>
2024-03-27 10:46:46 +0600

    Билд 0.1.1

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ API_TOKEN = '6723244609:AAG3YVvZyOHlknyNQmFu31paW_MUr9nkSkA'
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+try:
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher(bot)
+except Exception as e:
+    logging.error(f"Ошибка при создании объекта бота: {e}")
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -20,7 +23,8 @@ async def send_welcome(message: types.Message):
 async def send_info(message: types.Message):
     await message.reply("Это простой телеграм-бот, созданный с использованием aiogram.")
 
-
 if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+    try:
+        executor.start_polling(dp, skip_updates=True)
+    except Exception as e:
+        logging.error(f"Ошибка при запуске бота: {e}")
